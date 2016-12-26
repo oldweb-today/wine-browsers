b5fecb43f35a0be5fc74fd43f9aedaedf6786647
Ilya Kreymer <ikreymer@gmail.com>
2016-12-25 16:12:10 -0800

    owt-rename: use oldwebtoday/* prefix remove 'browser-' prefix, add 'tags' file instead of dir name for image name

---
+++ netscape-4.8/prefs.js
@@ -0,0 +1,12 @@
+user_pref("browser.related.enabled", false);
+user_pref("browser.startup.homepage_override", false);
+user_pref("browser.wfe.show_value", 3);
+user_pref("taskbar.floating", false);
+user_pref("browser.background_color", "#FFFFFF");
+user_pref("browser.startup.homepage", "about:");
+user_pref("browser.url_history.clear", 1);
+user_pref("browser.wfe.use_windows_colors", false);
+user_pref("custtoolbar.personal_toolbar_folder", "Personal Toolbar Folder");
+user_pref("custtoolbar.Browser.Personal_Toolbar.showing", false);
+user_pref("security.warn_submit_insecure", false);
+
