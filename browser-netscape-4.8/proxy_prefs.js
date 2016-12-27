1cfe1b539ab8233c99714df636c19aa924556b25
Ilya Kreymer <ikreymer@users.noreply.github.com>
2016-12-27 15:48:53 -0800

    owt-rename: (#2)
    use oldwebtoday/* prefix
    remove 'browser-' prefix, add 'tags' file instead of dir name for image name

--- browser-netscape-4.8/proxy_prefs.js
+++
@@ -1,5 +0,0 @@
-user_pref("network.proxy.http", "$PROXY_HOST");
-user_pref("network.proxy.http_port", $PROXY_PORT);
-user_pref("network.proxy.ssl", "$PROXY_HOST");
-user_pref("network.proxy.ssl_port", $PROXY_PORT);
-user_pref("network.proxy.type", 1);
---
+++ netscape-4.8/run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+export WINEDLLOVERRIDES="mshtml="
+export WINEPREFIX="/home/browser/.wine"
+
+USER_REG=/home/browser/custom_user.reg
+
+sed -i s/'$DIMENSION'/$SCREEN_WIDTH"x"$SCREEN_HEIGHT/g $USER_REG
+
+sudo chown browser /home/browser/prefs.js
+echo "user_pref(\"browser.window_rect\", \"0,0,$SCREEN_WIDTH,$SCREEN_HEIGHT\");" >> /home/browser/prefs.js
+
+if [[ -n "$PROXY_HOST" ]]; then
+    sudo chown browser /home/browser/proxy_prefs.js
+    sed -i s/'$PROXY_HOST'/$PROXY_HOST/g /home/browser/proxy_prefs.js
+    sed -i s/'$PROXY_PORT'/$PROXY_PORT/g /home/browser/proxy_prefs.js
+    cat /home/browser/proxy_prefs.js >> /home/browser/prefs.js
+fi
+
+cp /home/browser/prefs.js $WINEPREFIX/drive_c/Program\ Files/Netscape/Users/webrecorder/prefs.js
+
+wine regedit $USER_REG
+
+run_browser wine 'C:/Program Files/Netscape/Communicator/Program/netscape.exe' $URL
+
